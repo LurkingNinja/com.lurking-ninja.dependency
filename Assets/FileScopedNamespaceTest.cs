bf750528df3a7934b9b814b4030a3cb806e8c636
burak-efe <82805019+burak-efe@users.noreply.github.com>
2024-02-10 12:33:22 +0300

    add the file scoped namespace

--- Assets/FileScopedNamespaceTest.cs
+++ Assets/FileScopedNamespaceTest.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using LurkingNinja.Attributes;
 using UnityEngine;
 
+namespace TestNameSpace;
+
 [GenerateOnValidate]
 public partial class FileScopedNamespaceTest : MonoBehaviour
 {
-    [Get ]public Transform Trs;
-
+    [Get] public Transform Trs;
 }
